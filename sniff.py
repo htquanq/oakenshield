3c11e6c975063574741870090da60dee4984929c
quanght55 <htq0505@gmail.com>
2018-10-08 11:49:37 +0700

    Remove sniff_log.py

--- sniff.py
+++ sniff.py
@@ -1,6 +1,6 @@
 from scapy.all import *
-import netifaces, threading, logging
-from sniff_log import *
+import netifaces, threading, logging, time
+import logging.handlers as handlers
 
 def all_nics():
 	return netifaces.interfaces()
@@ -19,15 +19,35 @@ def pkt_callback(pkt):
 		tcp_dport=pkt[TCP].dport
 	if TCP in pkt:
 		data=pkt[TCP].payload
-	print " IP src : " + str(ip_src) + " TCP sport " + str(tcp_sport) + " payload " + str(data)
-	print " IP dst : " + str(ip_dst) + " TCP dport " + str(tcp_dport)
+	log_to_file("/tmp/test/test.log",ip_src + str(data), "sqli")
 
+def create_log_folder(interface):
+	path = "/tmp/" + interface +time.strftime("/%Y/%m/")
+	if not os.path.exists(path):
+		os.makedirs(path)
+	if not os.path.isfile(path+time.strftime("%d.log")):
+		file = open(path+time.strftime("%d.log"), "w+")
 
+def log_to_file(path,payload,name):
+    # Set event log name
+    logger = logging.getLogger(name)
+    # Set log format
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
+    # Set log file
+    fh = logging.FileHandler(path)
+    # Set log level
+    fh.setLevel(logging.WARN)
+    # Set log format
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    # Generate log data
+    logger.warn(payload)
 
 if __name__=="__main__":
 	nics=all_nics()
 	for interface in nics:
-		th = threading.Thread(
-      target=sniff(iface=interface, prn=pkt_callback, filter="tcp", store=0)
-    )
-		th.start()
+		create_log_folder(str(interface))
+		#th = threading.Thread(
+      #target=sniff(iface=interface, prn=pkt_callback, filter="tcp", store=0)
+    #)
+	#	th.start()
