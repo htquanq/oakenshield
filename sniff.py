38c1a59da11a4d1f65652190bd421cf723e2e1ca
quanght55 <htq0505@gmail.com>
2018-10-18 10:26:02 +0700

    Add nmap stealth scan detection

--- sniff.py
+++ sniff.py
@@ -12,10 +12,41 @@ INTERFACE=""
 def all_nics():
 	return netifaces.interfaces()
 
+def pkt_test(pkt):
+	print pkt.show()
+
 def pkt_callback(pkt):
+	opened = []
+	closed = []
 	create_log_folder(INTERFACE)
 	if pkt["IP"].get_field('proto').i2s[pkt.proto] == "icmp":
 		pingOfDeath(pkt)
+	# TCP packets
+	# Can be SQLi or Nmap scanner
+	# Detect NMap SYN Stealth scan for opened port and closed port
+	else:
+		src_port = pkt["TCP"].sport
+		dst_port = pkt["TCP"].dport
+		flags = pkt["TCP"].flags
+		seq = pkt["TCP"].seq
+		ack = pkt["TCP"].ack
+		# First request is SYN
+		# Nmap scan for open port
+		# SYN.seq: n (n is integer)
+		# SYN/ACK.ack = n + 1
+		# R.seq = n + 1
+
+		#Nmap scan for closed port
+		# SYN.seq
+		if flags == "S":
+			opened.append(pkt)
+			closed.append(pkt)
+		elif (flags=="SA") and (ack == opened[0]["TCP"].seq + 1) and (src_port == opened[0]["TCP"].dport):
+			opened.append(pkt)
+		elif (flags=="RA") and (ack == opened[0]["TCP"].seq + 1) and (dst_port == closed[0]["TCP"].sport):
+			closed.append(pkt)
+		elif (flags=="R") and (seq == opened[0]["TCP"].seq + 1) and (dst_port == opened[0]["TCP"].dport):
+			opened.append(pkt)
 
 
 #def synScan(flags, src_ip, dest_port, num):
@@ -25,6 +56,7 @@ def pkt_callback(pkt):
 
 def pingOfDeath(packet):
 	# Detect attempt to perform ping of death base on data size
+	# If packet size is more than 1500 bytes, log it
 	if packet["IP"].len > 1500:
 		ip_src=packet["IP"].src
 		ip_dst=packet["IP"].dst
@@ -66,4 +98,4 @@ if __name__=="__main__":
     #	)
 	#	th.start()
 	INTERFACE="lo"
-	sniff(iface=INTERFACE,prn=pkt_callback,filter="", store=0)
+	sniff(iface=INTERFACE,prn=pkt_test,filter="", store=0)
